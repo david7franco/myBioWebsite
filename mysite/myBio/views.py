b9f30abaea8a020349b74c371bd6fefc92e3b765
David Franco <david7franco@Outlook.com>
2024-07-25 15:59:36 -0500

    added pictures, fixed projects portion of website

--- mysite/myBio/views.py
+++ mysite/myBio/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+# i need to also make a forms to fill out so people can contact
