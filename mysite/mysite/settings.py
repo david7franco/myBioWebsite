80b04e586c3fc64079e5279800c5f3cddaf08c2e
David Franco <david7franco@Outlook.com>
2024-07-26 01:10:12 -0500

    static root config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,6 +127,7 @@ EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'myBio/static']
 
